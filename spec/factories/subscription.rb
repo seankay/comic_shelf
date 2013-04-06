9ede43a4982396e86bc960bb849888034dbf4caf
seankay <f.sean.kay@gmail.com>
2013-04-06 16:59:42 -0400

    Add subscription_with_card factory

--- spec/factories/subscription.rb
+++ spec/factories/subscription.rb
@@ -6,4 +6,12 @@ FactoryGirl.define do
     plan
     store
   end
+  factory :subscription_without_card do
+    sequence :email do |n|
+      "test#{n}@example.com"
+    end
+    stripe_card_token = nil
+    plan
+    store
+  end
 end
