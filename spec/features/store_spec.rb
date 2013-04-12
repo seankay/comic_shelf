20d12db07fe288465aa35355c0d890d2c9bf6e70
seankay <f.sean.kay@gmail.com>
2013-04-11 21:14:23 -0400

    Fixes #29

--- spec/features/store_spec.rb
+++ spec/features/store_spec.rb
@@ -4,8 +4,22 @@ describe "Store" do
   
   subject { page }
 
-  describe "Page" do
+  let(:subscription){ FactoryGirl.create(:subscription) }
+  let(:new_subscription){ FactoryGirl.create(:subscription_with_card) }
+
+  describe "Page", :vcr, :record => :new_episodes do
+    before do
+      seed_plans
+      register_user FactoryGirl.build(:user, :store_id => FactoryGirl.create(:store).id)
+      subscription.save_without_payment
+      subscription.update_subscription new_subscription
+    end
     it "should not show dashboard selection for non-admins"
     it "should show dashboard selection for non-admins"
+    it "should show subscribe link if current subscription is not active or pending cancelation" do
+      subscription.cancel_subscription
+      should have_link("Subscribe")
+    end
+
   end
 end
